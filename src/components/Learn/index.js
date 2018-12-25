ad3956b0602d9dc0f87e4baf004ed0a4e98e4c6c
Alexi Christakis <alexi.christakis@gmail.com>
2018-12-24 16:46:54 -0800

    some stuff to learn

--- src/components/Learn/index.js
+++ src/components/Learn/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { Element } from "react-scroll";
+import { Parallax } from "react-scroll-parallax";
 
 import "./learn.scss";
+
+const stack = [
+	{ title: "React", img: require("../../assets/logos/reactLogo.png") },
+	{ title: "Figma", img: require("../../assets/logos/figmaLogo.png") },
+	{ title: "AWS", img: require("../../assets/logos/amazonLogo.png") },
+	{ title: "Firebase", img: require("../../assets/logos/firebaseLogo.png") },
+	{ title: "Redux", img: require("../../assets/logos/reduxLogo.png") },
+	{ title: "Sentry", img: require("../../assets/logos/sentryLogo.png") },
+	{ title: "Slack", img: require("../../assets/logos/slackLogo.png") },
+	{ title: "Stripe", img: require("../../assets/logos/stripeLogo.png") }
+];
+
+const Tech = ({ title, img }) => (
+	<div className="tech-container">
+		<img className="logo" src={img} />
+		<div className="tech-name">{title}</div>
+	</div>
+);
 
 const Learn = props => {
 	return (
 		<Element className="section-container" id="learn" name="learn-section">
 			Learn
+			<div className={"stack-container"}>
+				{stack.map(item => (
+					<Tech {...item} />
+				))}
+			</div>
 		</Element>
 	);
 };
