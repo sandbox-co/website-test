c03db174d0d98ba8945305224fe9bf1b8ec6e408
Bryce Bjork <brycedbjork@gmail.com>
2019-01-16 11:05:50 -0500

    partners subsections

--- src/components/Partners/index.js
+++ src/components/Partners/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Element } from "react-scroll";
 
 import Header from "../universal/Section";
+import partners1 from "./partners1.svg";
+import partners2 from "./partners2.svg";
 
 const partners = [
 	{
@@ -26,10 +28,29 @@ const Partners = props => {
 	return (
 		<Element className="section-container" name="partners-section">
 			<Header title="sandbox partners" subtitle="we're powered by the best" />
+
+			<div className="subsection-wrapper">
+				<img className="subsection-icon" src={partners1} />
+				<div className="subsection-subtitle">
+					Our partners give us technical
+					<br />
+					mentors and sponsor our events
+				</div>
+			</div>
+
 			<div className={"stack-container"}>
 				{partners.map((item, i) => (
 					<Tech key={i} i={i} {...item} />
 				))}
+			</div>
+
+			<div className="subsection-wrapper">
+				<img className="subsection-icon" src={partners2} />
+				<div className="subsection-subtitle">
+					We give back to our partners
+					<br />
+					because they help us a lot
+				</div>
 			</div>
 		</Element>
 	);
