c4c1fccb3259cf30b00287e9cad799ddea72b57a
Alexi Christakis <alexi.christakis@gmail.com>
2019-01-15 19:50:03 -0500

    fix scroll arrow

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Element } from "react-scroll";
 import { Parallax } from "react-scroll-parallax";
+import { Link } from "react-scroll";
+import { FaChevronDown } from "react-icons/fa";
 
 import "./about.scss";
 
@@ -12,14 +14,21 @@ import Dots from "./Dots";
 
 const About = ({ width, height }) => (
 	<Element className="section-container" id="about" name="about-section">
+		<Parallax offsetYMax={"400px"} offsetYMin={"-600px"}>
+			<Link
+				className="bouncy-div"
+				to="about-section"
+				spy={true}
+				smooth={true}
+				// offset={50}
+				duration={500}
+			>
+				<FaChevronDown className="chevron" />
+			</Link>
+		</Parallax>
 		<Gradient width={width} height={height} />
 		<Dots width={width} height={height} />
-		<Parallax
-			className="catch-phrase"
-			offsetYMax={"100px"}
-			offsetYMin={"-400px"}
-			slowerScrollRate
-		>
+		<Parallax className="catch-phrase" offsetYMax={"100px"} offsetYMin={"-400px"} slowerScrollRate>
 			we turn creative students
 			<br />
 			into technical superstars
